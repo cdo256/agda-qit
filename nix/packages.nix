cbc6bad7e2eef63ca9107f34e88280c90ed9f60f
Christina O'Donnell <cdo@mutix.org>
2025-12-19 17:24:38 +0000

    wip: Add dissertation files.

--- nix/packages.nix
+++ nix/packages.nix
@@ -16,3 +16,45 @@
       };
     };
 }
+{ inputs, ... }:
+{
+  perSystem =
+    { system, pkgs, ... }:
+    {
+      config.packages = rec {
+        agda-base = pkgs.agda;
+        agda = agda-base.withPackages (ps: [
+          ps.standard-library
+          ps.cubical
+        ]);
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-medium
+            latexmk
+            standalone
+            pgf
+            amsmath
+            biblatex
+            tikz-cd
+            latex-bin
+            minted
+            #ifxetex
+            #ifluatex
+            xifthen
+            xcolor
+            polytable
+            etoolbox
+            environ
+            #xparse
+            xkeyval
+            ifmtarg
+            lazylist
+            newunicodechar
+            catchfilebetweentags
+            titling
+            dirtree
+            ;
+        };
+      };
+    };
+}
